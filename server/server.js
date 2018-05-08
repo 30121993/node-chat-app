c84e4d195db30643db36b0c13c4a5bd605bcf8a4
= <=>
2018-05-08 13:54:07 +0700

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -24,6 +24,18 @@ io.on('connection', (socket) => {
         console.log('createEmail', newEmail);
     });
 
+    // socket.emit from admin text Welcom to the chat app
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app'
+    });
+    // socket.broadcast.emit from Admin text New user joined
+    socket.broadcast.emit('newMessage',{
+        from: 'Admin',
+        text: 'New user joined',
+        createAt: new Date().getTime()
+    })
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
         io.emit('newMessage', {
@@ -31,6 +43,12 @@ io.on('connection', (socket) => {
             text: message.text,
             createAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
