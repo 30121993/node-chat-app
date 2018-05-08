6dfb566f5801b779f5e95e16c269049f945384aa
= <=>
2018-05-08 14:26:01 +0700

    create generateMessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,18 @@
+var expect = require('expect');
+
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () => {
+        var from = 'Long';
+        var text = 'Some message';
+        var message = generateMessage(from, text);
+
+        expect(typeof message.createdAt).toBe('number');
+        expect(message).toMatchObject({from, text});
+        // store res in variable
+        // assert from match
+        // assert text match
+        // assert createdAt is number
+    });
+});
